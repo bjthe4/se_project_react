bd5a399639e7c74c7b422b05e7e61c72fbfc7619
bjthe4 <bjthe4@gmail.com>
2024-08-04 18:35:31 -0500

    preview pop up issues

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -4,17 +4,87 @@ import viteLogo from "/vite.svg";*/
 import "./App.css";
 import Header from "./Header/Header";
 import Main from "../Main/Main.jsx";
+import ModalWithForm from "./ModalWithForm/ModalWithForm";
+import ItemModal from "../ItemModal/itemModal";
 
 function App() {
   const [weatherData, setWeatherData] = useState({ type: "cold" });
+  const [activeModal, setActiveModal] = useState("");
+  const [selectedCard, setSelectedCard] = useState({});
+
+  const handleCardClick = (card) => {
+    setActiveModal("preview");
+    setSelectedCard(card);
+  };
+
+  const handleAddClick = () => {
+    setActiveModal("add-garment");
+  };
+
+  const closeActiveModal = () => {
+    setActiveModal("");
+  };
 
   return (
     <>
       <div className="page">
         <div className="page__content">
-          <Header />
-          <Main weatherData={weatherData} />
+          <Header handleAddClick={handleAddClick} />
+          <Main weatherData={weatherData} handleCardClick={handleCardClick} />
         </div>
+        <ModalWithForm
+          title="New garment"
+          buttonText="Add garment"
+          activeModal={activeModal}
+          onClose={closeActiveModal}
+        >
+          <label htmlFor="name" className="modal__label">
+            Name
+            <input
+              type="text"
+              className="modal__input"
+              id="name"
+              placeholder="Name"
+            />
+          </label>
+          <label htmlFor="imageURL" className="modal__label">
+            Image {""}
+            <input
+              type="text"
+              className="modal__input"
+              id="imageURL"
+              placeholder="Image URL"
+            />
+          </label>
+          <fieldset className="modal__radio-buttons">
+            <legend className="modal__legend">Select the weather type </legend>
+            <label
+              htmlFor="hot"
+              className="modal__label modal__label__type_radio"
+            >
+              <input id="hot" type="radio" className="modal__radio-input" /> Hot
+            </label>
+            <label
+              htmlFor="warm"
+              className="modal__label modal__label__type_radio"
+            >
+              <input id="warm" type="radio" className="modal__radio-input" />{" "}
+              Warm
+            </label>
+            <label
+              htmlFor="cold"
+              className="modal__label modal__label__type_radio"
+            >
+              <input id="cold" type="radio" className="modal__radio-input" />{" "}
+              Cold
+            </label>
+          </fieldset>
+        </ModalWithForm>
+        <ItemModal
+          activeModal={activeModal}
+          card={selectedCard}
+          onClose={closeActiveModal}
+        />
       </div>
     </>
   );
--- src/components/App/Header/Header.jsx
+++ src/components/App/Header/Header.jsx
@@ -2,12 +2,18 @@ import "./Header.css";
 import headerLogo from "../../../assets/Logo.svg";
 import avatar from "../../../assets/Avatar.png";
 
-function Header() {
+function Header({ handleAddClick }) {
   return (
     <header className="header">
       <img className="header__logo" src={headerLogo} alt="Header logo" />
       <p className="header__date-and-location">Date, Location</p>
-      <button className="header__add-clothes-btn">+ Add Clothes</button>
+      <button
+        onClick={handleAddClick}
+        type="button"
+        className="header__add-clothes-btn"
+      >
+        + Add Clothes
+      </button>
       <div className="Header__user-container">
         <p className="Header__username">Benedict Iroha</p>
         <img src={avatar} alt="Benedict Iroha" className="Header__avatar" />
